5514d5659a1fa6447648340b01d632c030b11136
ciavarraelda-web <ciavarraelda@gmail.com>
2025-09-01 04:23:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'client'), // la cartella dove si trova index.html
   plugins: [react()],
   build: {
+    outDir: path.resolve(__dirname, 'dist'), // dove mettere i file buildati
+    emptyOutDir: true,
     rollupOptions: {
-      external: ["express", "class-variance-authority"],
+      input: path.resolve(__dirname, 'client/index.html'),
     },
   },
-  server: {
-    port: 5173,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src'),
+    },
   },
-});
+})
