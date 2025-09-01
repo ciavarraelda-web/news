a7a99e6412d075f3327fd5a2115950f5706069cf
ciavarraelda-web <ciavarraelda@gmail.com>
2025-09-01 02:08:17 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 export default defineConfig({
   content: ["./client/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [require("tailwindcss-animate")],
 });
